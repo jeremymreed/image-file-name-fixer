c60d305ce7f7416493b9c2a26eb6df67505b9adf
Jeremy Reed <reeje76@gmail.com>
2023-05-21 11:03:18 -0400

    Move file processing logic to processor.
    This logic doesn't need to be in main(), it should be in
    processor::process_file.
    
    We're still assuming that we're only processing a single file, and
    ignoring directories for now.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use image::GenericImageView;
-use image::ImageFormat;
-use image::io::Reader;
 use shadow_rs::shadow;
-
 mod arg_parser;
 mod config;
 mod processor;
@@ -16,27 +12,5 @@ fn main() {
 
     //let img = image::open(config.file_name).unwrap();
 
-    let reader = Reader::open(config.absolute_path).unwrap()
-        .with_guessed_format()
-        .expect("Failed to open image file");
-
-    println!("Format: {:?}", reader.format());
-    let format = match reader.format() {
-        Some(format) => format,
-        None => panic!("No format"),
-    };
-
-    let test = match format {
-        ImageFormat::Png => String::from("png"),
-        ImageFormat::Jpeg => String::from("jpeg"),
-        ImageFormat::Gif => String::from("gif"),
-        ImageFormat::WebP => String::from("webp"),
-        _ => panic!("Unspported format"),
-    };
-
-    println!("Format: {}", test);
-
-    let img = reader.decode().expect("Failed to read image");
-
-    println!("Dimensions: {:?}", img.dimensions());
+    processor::process_file(&config.absolute_path);
 }
--- src/processor.rs
+++ src/processor.rs
@@ -1,7 +1,36 @@
 use std::fs;
+use image::GenericImageView;
+use image::ImageFormat;
+use image::io::Reader;
 
 pub fn process_path(path: &String) -> String {
     let absolute_path: String = fs::canonicalize(path).unwrap().display().to_string();
 
     absolute_path
+}
+
+pub fn process_file(absolute_path: &String) {
+    let reader = Reader::open(absolute_path).unwrap()
+        .with_guessed_format()
+        .expect("Failed to open image file");
+
+    println!("Format: {:?}", reader.format());
+    let format = match reader.format() {
+        Some(format) => format,
+        None => panic!("No format"),
+    };
+
+    let test = match format {
+        ImageFormat::Png => String::from("png"),
+        ImageFormat::Jpeg => String::from("jpeg"),
+        ImageFormat::Gif => String::from("gif"),
+        ImageFormat::WebP => String::from("webp"),
+        _ => panic!("Unspported format"),
+    };
+
+    println!("Format: {}", test);
+
+    let img = reader.decode().expect("Failed to read image");
+
+    println!("Dimensions: {:?}", img.dimensions());
 }
