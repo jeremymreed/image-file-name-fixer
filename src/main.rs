b2d2f05c4007266c722490bb0407e68d679193cb
Jeremy Reed <reeje76@gmail.com>
2023-05-21 10:19:06 -0400

    Use new processor module.
    Also examine how we're going to determine what the image's format
    actually is.  This will be needed if we are going to fix the extension.
    
    We cannot use image::open(), since in this approach, the image library
    will assume the format from the file's extension.  We don't want this.
    
    We'll use a Reader, and use with_guessed_format() to read the image
    header to get the actual format.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
 use image::GenericImageView;
+use image::ImageFormat;
+use image::io::Reader;
 use shadow_rs::shadow;
 
 mod arg_parser;
 mod config;
-mod check_path;
+mod processor;
 
 shadow!(build);
 
 fn main() {
     let config: config::Config = arg_parser::parse_args();
 
-    match check_path::is_valid_path(&config.file_name) {
-        Ok(result) => if result {
-            println!("Valid path!");
-        } else {
-            println!("Invalid path!");
-        },
-        Err(message) => println!("Got error: {}", message),
-    }
+    processor::process_path(&config.file_name);
 
-    let img = image::open(config.file_name).unwrap();
+    //let img = image::open(config.file_name).unwrap();
+
+    let reader = Reader::open(config.file_name).unwrap()
+        .with_guessed_format()
+        .expect("Failed to open image file");
+
+    println!("Format: {:?}", reader.format());
+    let format = match reader.format() {
+        Some(format) => format,
+        None => panic!("No format"),
+    };
+
+    let test = match format {
+        ImageFormat::Png => String::from("png"),
+        ImageFormat::Jpeg => String::from("jpeg"),
+        ImageFormat::Gif => String::from("gif"),
+        ImageFormat::WebP => String::from("webp"),
+        _ => panic!("Unspported format"),
+    };
+
+    println!("Format: {}", test);
+
+    let img = reader.decode().expect("Failed to read image");
 
     println!("Dimensions: {:?}", img.dimensions());
 }
